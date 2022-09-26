163f6b02a8717d2c43a2fc5490cb9053803cb553
Tato <tato@Tatos-iMac.local>
2022-09-27 00:54:02 +0200

    added inling svg

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/assets/');
     eleventyConfig.addWatchTarget('./src/assets/');
+
     return {
         dir: {
             input: "src",

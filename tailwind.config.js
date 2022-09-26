711bbc88ae3db7bdcfda671fb28815a195be5f63
Tato <tato@Tatos-iMac.local>
2022-09-26 14:14:06 +0200

    added styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["**/*.njk"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sans": ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
